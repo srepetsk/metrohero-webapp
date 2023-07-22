a4d865ede4b87ec3c35bea09aeab05e07615a291
Stephen Repetski <stephen+github@srepetsk.net>
2023-07-21 22:54:38 -0400

    Add Dependabot

--- src/components/Line.js
+++ src/components/Line.js
@@ -720,33 +720,6 @@ export default createReactClass({
       stations = yellow_stations.Stations;
       lineContent = (
         <div ref='anchor' className='colored-line-content'>
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"Greenbelt"} stationCode={"E10"} ref="E10" onClick={this._handleClickStation.bind(null, "Greenbelt", "E10")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "Greenbelt", "E10")} onTapOutage={this._handleTapOutage.bind(null, "Greenbelt", "E10")} />
-          <BetweenStationRow code={'E10_E09'} isOnLeft={true} />
-          <BetweenStationRow code={'E09_E10'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"College Park – University of Maryland"} stationCode={"E09"} ref="E09" onClick={this._handleClickStation.bind(null, "College Park – University of Maryland", "E09")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "College Park – University of Maryland", "E09")} onTapOutage={this._handleTapOutage.bind(null, "College Park – University of Maryland", "E09")} />
-          <BetweenStationRow code={'E09_E08'} isOnLeft={true} />
-          <BetweenStationRow code={'E08_E09'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"Hyattsville Crossing"} stationCode={"E08"} ref="E08" onClick={this._handleClickStation.bind(null, "Hyattsville Crossing", "E08")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "Hyattsville Crossing", "E08")} onTapOutage={this._handleTapOutage.bind(null, "Hyattsville Crossing", "E08")} />
-          <BetweenStationRow code={'E08_E07'} isOnLeft={true} />
-          <BetweenStationRow code={'E07_E08'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"West Hyattsville"} stationCode={"E07"} ref="E07" onClick={this._handleClickStation.bind(null, "West Hyattsville", "E07")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "West Hyattsville", "E07")} onTapOutage={this._handleTapOutage.bind(null, "West Hyattsville", "E07")} />
-          <BetweenStationRow code={'E07_E06'} isOnLeft={true} />
-          <BetweenStationRow code={'E06_E07'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"Fort Totten"} stationCode={"E06"} ref="E06" onClick={this._handleClickStation.bind(null, "Fort Totten", "E06")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "Fort Totten", "E06")} onTapOutage={this._handleTapOutage.bind(null, "Fort Totten", "E06")} fromLineCodes={this.lineCodes.RD} toLineCodes={this.lineCodes.RD} />
-          <BetweenStationRow code={'E06_E05'} isOnLeft={true} />
-          <BetweenStationRow code={'E05_E06'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"Georgia Avenue – Petworth"} stationCode={"E05"} ref="E05" onClick={this._handleClickStation.bind(null, "Georgia Avenue – Petworth", "E05")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "Georgia Avenue – Petworth", "E05")} onTapOutage={this._handleTapOutage.bind(null, "Georgia Avenue – Petworth", "E05")} />
-          <BetweenStationRow code={'E05_E04'} isOnLeft={true} />
-          <BetweenStationRow code={'E04_E05'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"Columbia Heights"} stationCode={"E04"} ref="E04" onClick={this._handleClickStation.bind(null, "Columbia Heights", "E04")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "Columbia Heights", "E04")} onTapOutage={this._handleTapOutage.bind(null, "Columbia Heights", "E04")} />
-          <BetweenStationRow code={'E04_E03'} isOnLeft={true} />
-          <BetweenStationRow code={'E03_E04'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"U Street"} stationCode={"E03"} ref="E03" onClick={this._handleClickStation.bind(null, "U Street", "E03")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "U Street", "E03")} onTapOutage={this._handleTapOutage.bind(null, "U Street", "E03")} />
-          <BetweenStationRow code={'E03_E02'} isOnLeft={true} />
-          <BetweenStationRow code={'E02_E03'} isOnLeft={false} />
-          <StationRow isDarkMode={this.props.isDarkMode} stationName={"Shaw – Howard University"} stationCode={"E02"} ref="E02" onClick={this._handleClickStation.bind(null, "Shaw – Howard University", "E02")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "Shaw – Howard University", "E02")} onTapOutage={this._handleTapOutage.bind(null, "Shaw – Howard University", "E02")} />
-          <BetweenStationRow code={'E02_E01'} isOnLeft={true} />
-          <BetweenStationRow code={'E01_E02'} isOnLeft={false} />
           <StationRow isDarkMode={this.props.isDarkMode} stationName={"Mount Vernon Square"} stationCode={"E01"} ref="E01" onClick={this._handleClickStation.bind(null, "Mount Vernon Square", "E01")} stationTwitterOnClick={this._handleClickStationTwitter.bind(null, "Mount Vernon Square", "E01")} onTapOutage={this._handleTapOutage.bind(null, "Mount Vernon Square", "E01")} />
           <BetweenStationRow code={'E01_F01'} isOnLeft={true} />
           <BetweenStationRow code={'F01_E01'} isOnLeft={false} />
